71b8c73c9082e96b25c50a0ce869643f0aac02d3
imabhishekpatel <abhishekpatel09871@gmail.com>
2024-06-02 23:02:45 +0530

    first commit

---
+++ user_management/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+# from django.db import models
+
+# class UserManager(BaseUserManager):
+#     def create_user(self, email, password=None):
+#         if not email:
+#             raise ValueError('Users must have an email address')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, password=None):
+#         user = self.create_user(email, password)
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+
+# class User(AbstractBaseUser):
+#     email = models.EmailField(unique=True, max_length=255)
+#     is_active = models.BooleanField(default=True)
+#     is_admin = models.BooleanField(default=False)
+
+#     objects = UserManager()
+
+#     USERNAME_FIELD = 'email'
+
+#     def __str__(self):
+#         return self.email
+
+#     def has_perm(self, perm, obj=None):
+#         return self.is_admin
+
+#     def has_module_perms(self, app_label):
+#         return self.is_admin
